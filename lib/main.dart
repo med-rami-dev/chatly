c30a8cca36f478166312440c6a38bc57e6cc6833
Mohamed Rami <rami63879@gmail.com>
2024-02-26 10:50:21 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chatly/firebase_options.dart';
+import 'package:chatly/pages/chat_page.dart';
+import 'package:chatly/pages/login_page.dart';
+import 'package:chatly/pages/resgister_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ScholarChat());
+} 
+
+class ScholarChat extends StatelessWidget {
+  const ScholarChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        LoginPage.id: (context) => const LoginPage(),
+        RegisterPage.id: (context) => const RegisterPage(),
+        ChatPage.id: (context) => ChatPage()
+      },
+      initialRoute: LoginPage.id,
+    );
+  }
+}
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomFormTextField extends StatelessWidget {
+  CustomFormTextField(
+      {super.key, this.hintText, this.onChanged, this.obscureText = false});
+  final Function(String)? onChanged;
+  final String? hintText;
+
+  bool? obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText!,
+      validator: (data) {
+        if (data!.isEmpty) {
+          return 'Field is required';
+        }
+        return null;
+      },
+      onChanged: onChanged,
+      
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.white,
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
